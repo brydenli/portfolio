4e79908b7119ad68b39acba9bdaf5ed36f49426f
Bryden Li <brydenli.97@gmail.com>
2021-04-30 18:11:17 -0600

    updated main page CSS

--- src/components/main.js
+++ src/components/main.js
@@ -17,15 +17,17 @@ export const Main = () => {
 						I am a Mechanical Engineering graduate from the University of
 						Calgary with a passion for software development, as well as a drive
 						to learn new concepts, programming languages, and frameworks. My
-						work experiences and team opportunities have aided the growth of my
-						soft-skills; however, my desire to pursue a software-development
-						career has propelled my self-learning and project-creation in this
-						field.
+						work experiences and team opportunities have provided a foundation
+						for my soft-skills; however, my desire to pursue a
+						software-development career has propelled my self-learning and
+						project-creation in this field.
 						<br />
 						<br />
-						Currently, I have mostly been working with React, Express, and
+						So far, my projects have mostly incorporated React, Express, and
 						MongoDB; however, I am looking to expand my foundation as a
 						developer to incorporate a more diverse variety of concepts.
+						Currently, I am learning Python and Django to diversify my
+						portfolio.
 						<br />
 						<br />
 						Please take a look at my projects and contact me for more
@@ -36,18 +38,20 @@ export const Main = () => {
 					</a>
 					<br />
 				</div>
-				<Link className='link project-button' to='/projects'>
-					<div>
-						<h1 className='project-button-font'>
-							Click to check out my projects!
-							{/* <div>
+				<div>
+					<Link className='link project-button' to='/projects'>
+						<div className='border-div'>
+							<h1 className='project-button-font'>
+								Click to check out my projects!
+								{/* <div>
 							<div className='d'></div>
 							<div className='e'></div>
 							<div className='f'></div>
 						</div> */}
-						</h1>
-					</div>
-				</Link>
+							</h1>
+						</div>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
